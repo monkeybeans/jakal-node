15a5c79533b8e3db9a830378c6e7df66add2e047
Herr Fisk <mac@noreply.com>
2017-04-17 18:36:20 +0200

    Typescript server skeleton

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
