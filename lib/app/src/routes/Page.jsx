3bc7334dc338fafceea86a0e47bcc5eebf0c8ff7
Herr Fisk <mac@noreply.com>
2018-02-03 22:19:18 +0100

    Fulfix for // in url

--- lib/app/src/routes/Page.jsx
+++ lib/app/src/routes/Page.jsx
@@ -8,12 +8,18 @@ import Identify from './views/Identify';
 import ErrorNotFound from './views/ErrorNotFound';
 // import { Navigation } from '../components/menu/Navigation';
 
+// @TODO; fix the // from the hosting server
 const App = props => (
   <Provider store={props.store}>
     <Switch>
       <Route exact path="/jakal-web-BETA/" component={Collaboration} />
+      <Route exact path="//jakal-web-BETA/" component={Collaboration} />
+
       <Route exact path="/jakal-web-BETA/stats" component={Statistics} />
+      <Route exact path="//jakal-web-BETA/stats" component={Statistics} />
+
       <Route exact path="/jakal-web-BETA/auth" component={Identify} />
+      <Route exact path="//jakal-web-BETA/auth" component={Identify} />
       <Route component={ErrorNotFound} />
     </Switch>
   </Provider>
